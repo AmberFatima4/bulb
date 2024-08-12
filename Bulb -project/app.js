a566555ef3ad8d4228526e650047e9789b345f9d
AmberFatima4 <emberfatima23@gmail.com>
2024-08-12 06:48:53 +0500

    task completed

---
+++ Bulb -project/app.js
@@ -0,0 +1,8 @@
+var img = document.getElementById("open");
+function oon(){
+    img.src = "images/images 2.png"
+    document.getElementById("close").disabled = false
+}
+function ooff(){
+    img.src ="images/images 1.png"
+}
